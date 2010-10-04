8c663c6b4509e8debb236851eb735673e81d7caa
ddeucher <ddeucher@38ac2094-77c1-42ba-995b-c18f1c0364f1>
2010-10-04 22:48:28 +0000

    SQL Upgrade Scripts
    git-svn-id: http://svn.code.sf.net/p/perfmon4j/code/trunk@133 38ac2094-77c1-42ba-995b-c18f1c0364f1

---
+++ base/src/sql/MSSQL-UpdateTables-1.0.2_to_1.1.0.sql
@@ -0,0 +1,40 @@
+/*
+ *	Copyright 2008, 2009, 2010 Follett Software Company 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ * 	Follett Software Company
+ * 	1391 Corporate Drive
+ * 	McHenry, IL 60050
+ * 
+*/
+
+/* ******************************************************************************
+Notes - This script contains the SQL required to update the Perfmon4j SQL
+tables to 1.1.0 FROM 1.0.2
+****************************************************************************** */
+
+ALTER TABLE dbo.P4JIntervalData 
+	ADD SQLMaxDuration INT NULL,
+		SQLMaxDurationSet DATETIME NULL,
+		SQLMinDuration INT NULL,
+		SQLMinDurationSet DATETIME NULL,
+		SQLAverageDuration DECIMAL(18, 2) NULL,
+		SQLStandardDeviation DECIMAL(18, 2) NULL,
+		SQLDurationSum BIGINT NULL,
+		SQLDurationSumOfSquares BIGINT NULL
+GO
+
+ALTER TABLE dbo.P4JThreadTrace
+	ADD SQLDuration INT NULL
+GO
