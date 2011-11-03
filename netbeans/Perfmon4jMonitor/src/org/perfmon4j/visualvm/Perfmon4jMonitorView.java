d7736c9c95f0cb9fa2ca3fbf6b3fdd6f2c8d8d45
ddeucher <ddeucher@38ac2094-77c1-42ba-995b-c18f1c0364f1>
2011-11-03 21:31:49 +0000

    Added Perfmon4j Copyright header
    git-svn-id: http://svn.code.sf.net/p/perfmon4j/code/trunk@199 38ac2094-77c1-42ba-995b-c18f1c0364f1

--- netbeans/Perfmon4jMonitor/src/org/perfmon4j/visualvm/Perfmon4jMonitorView.java
+++ netbeans/Perfmon4jMonitor/src/org/perfmon4j/visualvm/Perfmon4jMonitorView.java
@@ -1,7 +1,24 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+ *	Copyright 2011 Follett Software Company 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ * 	Follett Software Company
+ * 	1391 Corporate Drive
+ * 	McHenry, IL 60050
+ * 
+*/
+
 package org.perfmon4j.visualvm;
 
 import com.sun.tools.visualvm.application.Application;
