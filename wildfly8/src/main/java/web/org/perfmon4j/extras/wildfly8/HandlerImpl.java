c173e5a9c37a56c1d7949b033636aee6cf5c0aae
ddeucher <ddeucher@38ac2094-77c1-42ba-995b-c18f1c0364f1>
2014-10-21 23:20:54 +0000

    Added unit test to the HttpHandler Valve
    git-svn-id: http://svn.code.sf.net/p/perfmon4j/code/trunk@366 38ac2094-77c1-42ba-995b-c18f1c0364f1

--- wildfly8/src/main/java/web/org/perfmon4j/extras/wildfly8/HandlerImpl.java
+++ wildfly8/src/main/java/web/org/perfmon4j/extras/wildfly8/HandlerImpl.java
@@ -2,15 +2,21 @@ package web.org.perfmon4j.extras.wildfly8;
 
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HeaderMap;
+import io.undertow.util.HttpString;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.Deque;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import org.perfmon4j.PerfMon;
 import org.perfmon4j.PerfMonTimer;
 import org.perfmon4j.SQLTime;
+import org.perfmon4j.ThreadTraceConfig;
+import org.perfmon4j.ThreadTraceConfig.Trigger;
+import org.perfmon4j.UserAgentSnapShotMonitor;
 import org.perfmon4j.util.Logger;
 import org.perfmon4j.util.LoggerFactory;
 import org.perfmon4j.util.MiscHelper;
@@ -22,45 +28,99 @@ class HandlerImpl implements HttpHandler {
 	private final HttpHandler next;
 	private final String baseFilterCategory;
 	private final boolean outputRequestAndDuration;
+	private final boolean abortTimerOnImageResponse;
+	private final boolean abortTimerOnRedirect;
+	private final Pattern abortTimerOnURLPattern;
+	private final Pattern skipTimerOnURLPattern;
+	private static final HttpString CONTENT_TYPE = new HttpString("Content-Type");
 
 	HandlerImpl(PerfmonHandlerWrapper parent, HttpHandler next) {
 		this.parent = parent;
 		this.next = next;
 		this.baseFilterCategory = parent.getBaseFilterCategory();
 		this.outputRequestAndDuration = parent.isOutputRequestAndDuration();
+		this.abortTimerOnImageResponse = parent.isAbortTimerOnImageResponse();
+		this.abortTimerOnRedirect = parent.isAbortTimerOnRedirect();
+		this.abortTimerOnURLPattern = compilePattern(parent.getAbortTimerOnURLPattern());
+		this.skipTimerOnURLPattern = compilePattern(parent.getSkipTimerOnURLPattern());
+		
+		PerfMon.getMonitor(this.baseFilterCategory, false);  // Always create the base category monitor, all child monitors are only created when a monitor is attached.
+	}
+	
+	public void handleRequest(HttpServerExchange exchange) throws Exception {
+		boolean skip = false;
+		
+		if (skipTimerOnURLPattern != null) {
+			skip = skipTimerOnURLPattern.matcher(exchange.getRequestPath()).matches();
+		}
+		
+		if (skip) {
+			next.handleRequest(exchange);
+		} else {
+			doHandleRequest(exchange);
+		}
 	}
 
-	public void handleRequest(HttpServerExchange exchange) throws Exception {
+	
+	private void doHandleRequest(HttpServerExchange exchange) throws Exception {
         Long localStartTime = null;
         Long localSQLStartTime = null;
-		
-		
-		// TODO Auto-generated method stub
-System.out.println("RequestPath: " + exchange.getRequestPath());			
-System.out.println("QueryString: " + exchange.getQueryString());			
 
 		PerfMonTimer timer = null;
+		boolean pushedRequestValidator = false;
 		try {
-			timer = startTimerForRequest(exchange);
-	        if (outputRequestAndDuration) {
-	        	localStartTime = new Long(MiscHelper.currentTimeWithMilliResolution());
-	        	if (SQLTime.isEnabled()) {
-	        		localSQLStartTime = new Long(SQLTime.getSQLTime());
-	        	}
-	        }
-			next.handleRequest(exchange);
+        	if (PerfMon.hasHttpRequestBasedThreadTraceTriggers()) {
+        		ThreadTraceConfig.pushValidator(new RequestValidator(exchange));
+        		pushedRequestValidator = true;
+        	}			
+			
+			try {
+				timer = startTimerForRequest(exchange);
+		        if (outputRequestAndDuration) {
+		        	localStartTime = new Long(MiscHelper.currentTimeWithMilliResolution());
+		        	if (SQLTime.isEnabled()) {
+		        		localSQLStartTime = new Long(SQLTime.getSQLTime());
+		        	}
+		        }
+				next.handleRequest(exchange);
+			} finally {
+				boolean doAbort = false;
+				
+				if (!doAbort && abortTimerOnRedirect) {
+					doAbort = (exchange.getResponseCode() / 100) == 3;
+				}
+				
+	            if (!doAbort && abortTimerOnImageResponse) {
+	                String contentType = exchange.getResponseHeaders().get(CONTENT_TYPE, 0);
+	                doAbort = contentType != null && contentType.startsWith("image");
+	            }
+	            
+	            if (!doAbort && (abortTimerOnURLPattern != null)) {
+	            	Matcher m = abortTimerOnURLPattern.matcher(exchange.getRequestPath());
+	            	doAbort = m.matches();
+	            }
+				
+	            if (doAbort) {
+	            	PerfMonTimer.abort(timer);
+	            } else {
+	            	PerfMonTimer.stop(timer);
+	            	notifyUserAgentMonitor(exchange);
+		        	if (localStartTime != null) {
+		        		String sqlDurationStr = "";
+		        		if (localSQLStartTime != null) {
+		        			long sqlDuration = SQLTime.getSQLTime() - localSQLStartTime.longValue();
+		        			sqlDurationStr = "(SQL: " + sqlDuration + ")";
+		        		}
+		        		long duration = Math.max(MiscHelper.currentTimeWithMilliResolution() -
+		        			localStartTime.longValue(), 0);
+		        		logger.logInfo(duration + sqlDurationStr + " " + buildRequestDescription(exchange));
+		        	}
+	            }
+			}
 		} finally {
-			PerfMonTimer.stop(timer);
-        	if (localStartTime != null) {
-        		String sqlDurationStr = "";
-        		if (localSQLStartTime != null) {
-        			long sqlDuration = SQLTime.getSQLTime() - localSQLStartTime.longValue();
-        			sqlDurationStr = "(SQL: " + sqlDuration + ")";
-        		}
-        		long duration = Math.max(MiscHelper.currentTimeWithMilliResolution() -
-        			localStartTime.longValue(), 0);
-        		logger.logInfo(duration + sqlDurationStr + " " + buildRequestDescription(exchange));
-        	}
+			if (pushedRequestValidator) {
+				ThreadTraceConfig.popValidator();
+			}
 		}
 	}
 	
@@ -71,6 +131,9 @@ System.out.println("QueryString: " + exchange.getQueryString());
         	
         	String requestPath = exchange.getRequestPath();
         	if (requestPath != null) {
+        		if (requestPath.endsWith("/")) {
+        			requestPath = requestPath.substring(0, requestPath.length()-1);
+        		}
         		monitorCategory += requestPath.replaceAll("\\.", "_").replaceAll("/", "\\.");
         	}
             result = PerfMonTimer.start(monitorCategory, true);
@@ -90,7 +153,7 @@ System.out.println("QueryString: " + exchange.getQueryString());
     	return result.toString();
     }
     
-    private static final Pattern passwordParamPattern = Pattern.compile("[\\?|\\&]{1}password\\=([^\\&]*)");
+    private static final Pattern passwordParamPattern = Pattern.compile("[\\?|\\&]{1}password\\=([^\\&\\;]*)");
     
     
     static String maskPassword(String queryParams) {
@@ -111,17 +174,51 @@ System.out.println("QueryString: " + exchange.getQueryString());
     	return buffer.toString();
     }
     
-   
-    private static String encodeNoThrow(String value) {
-    	String result = value;
-    	try {
-			result = URLEncoder.encode(value, "UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			if (logger.isDebugEnabled()) {
-				logger.logDebug("Error: unable to encode string: \"" + value + "\"", e);
-			}
-		}
+    private void notifyUserAgentMonitor(HttpServerExchange exchange) {
+    	HeaderMap map  = exchange.getRequestHeaders();
+    	if (map != null) {
+    		String userAgentString = map.getFirst("User-agent");
+            if (userAgentString != null) {
+                UserAgentSnapShotMonitor.insertUserAgent(userAgentString);
+            }
+    	}
+    }
+    
+    private static Pattern compilePattern(String pattern) {
+    	ThreadTraceConfig x;
+    	Pattern result = null;
+    	if (pattern != null) {
+	        try {
+	        	result = Pattern.compile(pattern);
+	        } catch (PatternSyntaxException ex) {
+	            logger.logError("Error compiling pattern: " + pattern, ex);
+	        }
+    	}
     	return result;
     }
+
+    public static class RequestValidator implements ThreadTraceConfig.TriggerValidator {
+    	private final HttpServerExchange exchange;
+    	RequestValidator(HttpServerExchange exchange) {
+    		this.exchange = exchange;
+    	}
+    	
+		public boolean isValid(Trigger trigger) {
+			boolean result = false;
+			
+			if (trigger.getType() == ThreadTraceConfig.TriggerType.HTTP_REQUEST_PARAM) {
+				ThreadTraceConfig.HTTPRequestTrigger t = (ThreadTraceConfig.HTTPRequestTrigger)trigger;
+				
+				Map<String, Deque<String>> map = exchange.getQueryParameters();
+				if (map != null) {
+					Deque<String> values = map.get(t.getName());
+					result = values != null && values.contains(t.getValue());
+				}
+			}
+			return result;
+		}
+    }
+
+
 }
 
--- wildfly8/src/test/java/web/org/perfmon4j/extras/wildfly8/HandlerImplTest.java
+++ wildfly8/src/test/java/web/org/perfmon4j/extras/wildfly8/HandlerImplTest.java
@@ -1,23 +1,47 @@
 package web.org.perfmon4j.extras.wildfly8;
 
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.ServerConnection;
+import io.undertow.util.HttpString;
 import junit.framework.TestCase;
 
-//public class HandlerImplTest {
-//}
+import org.mockito.Mockito;
+import org.perfmon4j.Appender;
+import org.perfmon4j.IntervalData;
+import org.perfmon4j.PerfMon;
+import org.perfmon4j.PerfMonConfiguration;
+import org.perfmon4j.PerfMonData;
+import org.perfmon4j.ThreadTraceConfig;
+import org.perfmon4j.ThreadTraceConfig.Trigger;
+
 public class HandlerImplTest extends TestCase {
 
 	public HandlerImplTest(String name) {
 		super(name);
 	}
 
+	private PerfMonConfiguration config;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
-	}
-
-	protected void tearDown() throws Exception {
-		super.tearDown();
+		config = new PerfMonConfiguration();
+		config.defineAppender("SIMPLE", SimpleAppender.class.getName(), "100 ms");
+		config.defineMonitor("WebRequest");
+		config.attachAppenderToMonitor("WebRequest", "SIMPLE", ".");
+		
+		SimpleAppender.reset();
+		PerfMon.configure(config);
+		Thread.sleep(100);
 	}
 	
+	
+	protected void tearDown() throws Exception {
+		PerfMon.configure();
+		
+		super.tearDown();
+		Thread.sleep(100);
+	}
 	
 	public void testMaskPasswordInQueryString() {
 		assertEquals("Password only parameter", "?password=*******", HandlerImpl.maskPassword("?password=dave"));
@@ -25,6 +49,179 @@ public class HandlerImplTest extends TestCase {
 		assertEquals("Password middle parameter", "?user=dave&password=*******&time=now", HandlerImpl.maskPassword("?user=dave&password=dave&time=now"));
 		assertEquals("Multiple password parameters", "?password=*******&password=*******&password=*******&password=*******", HandlerImpl.maskPassword("?password=dave&password=this is a test&password=&password=t"));
 		assertEquals("Password in value is ignored", "?word=password", HandlerImpl.maskPassword("?word=password"));
+		assertEquals("Work with ; separator", "?password=*******;x=y", HandlerImpl.maskPassword("?password=dave;x=y"));
+	}
+	
+	public static class SimpleAppender extends Appender {
+		static SimpleAppender appender = null; 
+		
+		public SimpleAppender(AppenderID id) {
+			super(id);
+			appender = this;
+		}
+	
+		static void reset() {
+			completions = 0;
+			hits = 0;
+			threadTraces = 0;
+			
+		}
+		
+		static void flushOutput() throws Exception {
+			if (appender != null) {
+				Thread.sleep(100);
+				appender.flush();
+				Thread.sleep(100);
+			}
+		}
+
+		static int completions = 0;
+		static int hits = 0;
+		static int threadTraces = 0;
+		
+		@Override
+		public void outputData(PerfMonData data) {
+			if (data instanceof IntervalData) {
+				IntervalData d = (IntervalData)data;
+				completions += d.getTotalCompletions();
+				hits += d.getTotalHits();
+			} else {
+				threadTraces++;
+			}
+			System.out.println(data.toAppenderString());			
+		}
+	}
+	
+	public void testSimpleMonitor() throws Exception {
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		HttpHandler handler = buildMockHttpHandler();
+		HttpServerExchange exchange = buildMockExchange();
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		impl.handleRequest(exchange);
+		SimpleAppender.flushOutput();
+		
+		assertEquals(1, SimpleAppender.completions);
+		assertEquals("Should of had 1 hit", 1, SimpleAppender.hits);		
+	}
+	
+	public void testAbortTimerOnImageResponse() throws Exception {
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		wrapper.setAbortTimerOnImageResponse(true);
+		
+		HttpHandler handler = buildMockHttpHandler();
+		HttpServerExchange exchange = buildMockExchange();
+		exchange.getResponseHeaders().add(new HttpString("Content-Type"), "image/jpeg");
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		impl.handleRequest(exchange);
+		
+		SimpleAppender.flushOutput();
+		
+		assertEquals("Should of had 1 hit", 1, SimpleAppender.hits);
+		assertEquals("Completion should have been aborted", 0, SimpleAppender.completions);
+	}
+	
+	public void testAbortTimerOnRedirect() throws Exception {
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		wrapper.setAbortTimerOnRedirect(true);
+		
+		HttpHandler handler = buildMockHttpHandler();
+		HttpServerExchange exchange = buildMockExchange();
+		exchange.setResponseCode(303);
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		impl.handleRequest(exchange);
+		
+		SimpleAppender.flushOutput();
+		
+		assertEquals("Should of had 1 hit", 1, SimpleAppender.hits);
+		assertEquals("Completion should have been aborted", 0, SimpleAppender.completions);
 	}
 
+	public void testAbortTimerOnURLPattern() throws Exception {
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		wrapper.setAbortTimerOnURLPattern("/dave");
+		
+		HttpHandler handler = buildMockHttpHandler();
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		impl.handleRequest(buildMockExchange("/dave"));
+		impl.handleRequest(buildMockExchange("/notdave"));
+		
+		SimpleAppender.flushOutput();
+		
+		assertEquals("Both should have hit", 2, SimpleAppender.hits);
+		assertEquals("Only the request that did not match pattern should have been flagged complete", 
+				1, SimpleAppender.completions);
+	}
+	
+	public void testSkipTimerOnURLPattern() throws Exception {
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		wrapper.setSkipTimerOnURLPattern("/dave");
+		
+		HttpHandler handler = buildMockHttpHandler();
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		impl.handleRequest(buildMockExchange("/dave"));
+		impl.handleRequest(buildMockExchange("/notdave"));
+		
+		SimpleAppender.flushOutput();
+		
+		assertEquals("Only ther request that did not match pattern should have been flagged as a hit", 
+				1, SimpleAppender.hits);
+		assertEquals("Only the request that did not match pattern should have been flagged complete", 
+				1, SimpleAppender.completions);
+	}
+	
+	public void testThreadTraceHttpRequestTrigger() throws Exception {
+		Trigger trigger = new ThreadTraceConfig.HTTPRequestTrigger("userName", "Dave");
+		addThreadTraceTrigger(trigger);
+		
+		PerfmonHandlerWrapper wrapper = new PerfmonHandlerWrapper();
+		HttpHandler handler = buildMockHttpHandler();
+		HttpServerExchange exchange = buildMockExchange();
+		
+		HandlerImpl impl = new HandlerImpl(wrapper, handler);
+		
+		impl.handleRequest(exchange);
+		
+		// Now add a request parameter that matches the trigger.
+		exchange.addQueryParam("userName", "Dave");
+		impl.handleRequest(exchange);
+		
+		SimpleAppender.flushOutput();
+		
+		assertEquals("Should have gotten 1 thread trace", 1, SimpleAppender.threadTraces);
+	}
+	
+	private HttpHandler buildMockHttpHandler() {
+		HttpHandler handler = Mockito.mock(HttpHandler.class);
+		
+		return handler;
+	}
+	
+	private HttpServerExchange buildMockExchange() {
+		return buildMockExchange("/test");
+	}
+	
+	private HttpServerExchange buildMockExchange(String requestPath) {
+		ServerConnection conn = Mockito.mock(ServerConnection.class); 
+		HttpServerExchange result = new HttpServerExchange(conn);
+		result.setRequestPath(requestPath);
+		
+		return result;
+	}
+
+	private void addThreadTraceTrigger(Trigger trigger) throws Exception {
+		ThreadTraceConfig ttConfig = new ThreadTraceConfig();
+		if (trigger != null) {
+			ttConfig.setTriggers(new Trigger[]{trigger});
+		}
+		ttConfig.addAppender(config.getAppenderForName("SIMPLE"));
+		config.addThreadTraceConfig("WebRequest", ttConfig);
+		
+		PerfMon.configure(config);
+		Thread.sleep(100);
+	}
 }
