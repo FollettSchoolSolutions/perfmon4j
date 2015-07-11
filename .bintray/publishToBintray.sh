420b507a625aa9a235fa9041ae4bd9d9601838ed
David Deuchert <david@deuchert.com>
2015-07-11 14:52:05 -0500

    Changed from the echo to real api request to bintray

--- .bintray/publishToBintray.sh
+++ .bintray/publishToBintray.sh
@@ -10,6 +10,6 @@ fi
 
 for f in ./target/perfmon4j-project*.zip
 do
-	echo curl -T $f -ddeuchert:$BINTRAY_API_KEY https://api.bintray.com/content/fss-development/Perfmon4j/$BINTRAY_TARGET/$(basename "$f")
-	echo curl -XPOST -ddeuchert:$BINTRAY_API_KEY https://api.bintray.com/content/fss-development/Perfmon4j/$BINTRAY_TARGET/$(basename "$f")/publish
+	curl -T $f -ddeuchert:$BINTRAY_API_KEY https://api.bintray.com/content/fss-development/Perfmon4j/$BINTRAY_TARGET/$(basename "$f")
+	curl -XPOST -ddeuchert:$BINTRAY_API_KEY https://api.bintray.com/content/fss-development/Perfmon4j/$BINTRAY_TARGET/$(basename "$f")/publish
 done
