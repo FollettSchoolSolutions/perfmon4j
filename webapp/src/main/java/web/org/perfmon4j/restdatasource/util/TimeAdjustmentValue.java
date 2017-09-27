e9e4b1244238274d87bc8810cf5ec1b98460a706
David Deuchert <david@deuchert.com>
2017-09-27 17:25:46 -0500

    TimeAdjustmentValue
    The Time adjustment value will provide a quick and easy way to move a report range back in forth in time.  For instance if you have the following:
      From:  now-8H
      To: now
    ...and you wanted to see how the data looked yesterday at this time, you could simply resubmit the report with the following:
       From: now-8H {-1D}
       To: now

---
+++ webapp/src/main/java/web/org/perfmon4j/restdatasource/util/TimeAdjustmentValue.java
@@ -0,0 +1,108 @@
+/*
+ *	Copyright 2017 Follett School Solutions 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ * 	Follett School Solutions
+ * 	1391 Corporate Drive
+ * 	McHenry, IL 60050
+ * 
+*/
+package web.org.perfmon4j.restdatasource.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TimeAdjustmentValue {
+	private static final Pattern PATTERN = Pattern.compile("\\{([\\-\\+]?\\d*)([HDWM]?)\\}");
+	
+	public enum Period {
+		NOADJUSTMENT,
+		HOUR,
+		DAY,
+		WEEK,
+		MONTH
+	}
+
+	private final int increment;
+	private final Period period;
+	
+	public TimeAdjustmentValue(Period period, int increment) {
+		this.increment = increment;
+		this.period = period;
+	}
+
+	public int getIncrement() {
+		return increment;
+	}
+
+	public Period getPeriod() {
+		return period;
+	}
+
+	static public TimeAdjustmentValue parse(String input) {
+		TimeAdjustmentValue result = null;
+		if (input != null) {
+			Matcher m = PATTERN.matcher(input);
+			if (m.find()) {
+				if (m.groupCount() > 0 && !"".equals(m.group(1))) {
+					Period p = Period.DAY; // Default value;
+					int i = Integer.parseInt(m.group(1));
+					String g2 = m.group(2);
+					if ("H".equals(g2)) {
+						p = Period.HOUR;
+					} else if ("W".equals(g2)) {
+						p = Period.WEEK;
+					} else if ("M".equals(g2)) {
+						p = Period.MONTH;
+					}
+					result = new TimeAdjustmentValue(p, i);
+				} else {
+					result = new TimeAdjustmentValue(Period.NOADJUSTMENT, 0);
+				}
+			}
+		}
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return "TimeAdjustmentValue [increment=" + increment + ", period="
+				+ period + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + increment;
+		result = prime * result + ((period == null) ? 0 : period.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TimeAdjustmentValue other = (TimeAdjustmentValue) obj;
+		if (increment != other.increment)
+			return false;
+		if (period != other.period)
+			return false;
+		return true;
+	}
+}
---
+++ webapp/src/test/java/web/org/perfmon4j/restdatasource/util/TimeAdjustmentValueTest.java
@@ -0,0 +1,151 @@
+/*
+ *	Copyright 2017 Follett School Solutions 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ * 	Follett School Solutions
+ * 	1391 Corporate Drive
+ * 	McHenry, IL 60050
+ * 
+*/
+package web.org.perfmon4j.restdatasource.util;
+
+import junit.framework.TestCase;
+import web.org.perfmon4j.restdatasource.util.TimeAdjustmentValue.Period;
+
+public class TimeAdjustmentValueTest extends TestCase {
+	
+	public TimeAdjustmentValueTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testNothingToParse() throws Exception { 
+		assertNull("There is nothing here", TimeAdjustmentValue.parse(null));
+		assertNull("There is nothing here", TimeAdjustmentValue.parse(""));
+		assertNull("There is nothing here", TimeAdjustmentValue.parse("asfasdf"));
+		assertNull("There is nothing here", TimeAdjustmentValue.parse("{asfasdf}"));
+		assertNull("There is nothing here", TimeAdjustmentValue.parse("{asfasdf"));
+		assertNull("There is nothing here", TimeAdjustmentValue.parse("{asfasdf}"));
+	}
+	
+	/**
+	 * An empty "{}" pattern indicate that no adjustment should be applied to the specific date field.
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseNoOpAdjustment() throws Exception { 
+		TimeAdjustmentValue noAdjustment = new TimeAdjustmentValue(Period.NOADJUSTMENT, 0);
+		
+		assertEquals("No adjustment", noAdjustment, TimeAdjustmentValue.parse("{}"));
+		assertEquals("Date/Time value before is OK", noAdjustment, TimeAdjustmentValue.parse("now{}"));
+		assertEquals("Date/Time value before is OK with whitespace", noAdjustment, TimeAdjustmentValue.parse("now{}"));
+		assertEquals("Date/Time value after is OK", noAdjustment, TimeAdjustmentValue.parse("{}now"));
+		assertEquals("Date/Time value after is OK with whitespace", noAdjustment, TimeAdjustmentValue.parse("{} now"));
+	}
+	
+	/**
+	 * The default period will be Days.  The following values will result in adding 2 days to the 
+	 * specified time:
+	 * 	"2017-09-27T15:30 {2}"
+	 * 	"2017-09-27T15:30 {2D}"
+	 * 	"2017-09-27T15:30 {+2}"
+	 * 	"2017-09-27T15:30 {+2D}"
+	 * 
+	 * The following values will result in subtracting 2 days from the specified time:
+	 * 	"2017-09-27T15:30 {-2}"
+	 * 	"2017-09-27T15:30 {-2D}"
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseDays() throws Exception { 
+		TimeAdjustmentValue positive2Days = new TimeAdjustmentValue(Period.DAY, 2);
+		TimeAdjustmentValue negative2Days = new TimeAdjustmentValue(Period.DAY, -2);
+		
+		assertEquals("+sign allowed, period defaults to days", positive2Days, TimeAdjustmentValue.parse("{+2}"));
+		assertEquals("period defaults to days", positive2Days, TimeAdjustmentValue.parse("{2}"));
+		assertEquals("explicit period", positive2Days, TimeAdjustmentValue.parse("{2D}"));
+		assertEquals("period defaults to days", negative2Days, TimeAdjustmentValue.parse("{-2}"));
+		assertEquals("explicit period", negative2Days, TimeAdjustmentValue.parse("{-2D}"));
+	}
+	
+
+	/**
+	 * The following values will result in adding 2 hours to the 
+	 * specified time:
+	 * 	"2017-09-27T15:30 {2H}"
+	 * 	"2017-09-27T15:30 {+2H}"
+	 * 
+	 * The following values will result in subtracting 2 hours from the specified time:
+	 * 	"2017-09-27T15:30 {-2H}"
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseHours() throws Exception { 
+		TimeAdjustmentValue positive2 = new TimeAdjustmentValue(Period.HOUR, 2);
+		TimeAdjustmentValue negative2 = new TimeAdjustmentValue(Period.HOUR, -2);
+		
+		assertEquals("+sign allowed", positive2, TimeAdjustmentValue.parse("{+2H}"));
+		assertEquals("+sign not required", positive2, TimeAdjustmentValue.parse("{2H}"));
+		assertEquals("negative value", negative2, TimeAdjustmentValue.parse("{-2H}"));
+	}
+
+	/**
+	 * The following values will result in adding 2 weeks to the 
+	 * specified time:
+	 * 	"2017-09-27T15:30 {2W}"
+	 * 	"2017-09-27T15:30 {+2W}"
+	 * 
+	 * The following values will result in subtracting 2 weeks from the specified time:
+	 * 	"2017-09-27T15:30 {-2W}"
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseWeeks() throws Exception { 
+		TimeAdjustmentValue positive2 = new TimeAdjustmentValue(Period.WEEK, 2);
+		TimeAdjustmentValue negative2 = new TimeAdjustmentValue(Period.WEEK, -2);
+		
+		assertEquals("+sign allowed", positive2, TimeAdjustmentValue.parse("{+2W}"));
+		assertEquals("+sign not required", positive2, TimeAdjustmentValue.parse("{2W}"));
+		assertEquals("negative value", negative2, TimeAdjustmentValue.parse("{-2W}"));
+	}
+
+
+	/**
+	 * The following values will result in adding 2 months to the 
+	 * specified time:
+	 * 	"2017-09-27T15:30 {2M}"
+	 * 	"2017-09-27T15:30 {+2M}"
+	 * 
+	 * The following values will result in subtracting 2 months from the specified time:
+	 * 	"2017-09-27T15:30 {-2M}"
+	 * 
+	 * @throws Exception
+	 */
+	public void testParseMonths() throws Exception { 
+		TimeAdjustmentValue positive2 = new TimeAdjustmentValue(Period.MONTH, 2);
+		TimeAdjustmentValue negative2 = new TimeAdjustmentValue(Period.MONTH, -2);
+		
+		assertEquals("+sign allowed", positive2, TimeAdjustmentValue.parse("{+2M}"));
+		assertEquals("+sign not required", positive2, TimeAdjustmentValue.parse("{2M}"));
+		assertEquals("negative value", negative2, TimeAdjustmentValue.parse("{-2M}"));
+	}
+
+}
