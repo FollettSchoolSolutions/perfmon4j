ee2d0bb2037bb2139af2540b14b1875fc05f78ce
David Deuchert <david@deuchert.com>
2018-11-10 11:19:49 -0600

    Reduced stacktrace logging unless debug level specified.

--- base/src/main/java/org/perfmon4j/demo/JVMMemory.java
+++ base/src/main/java/org/perfmon4j/demo/JVMMemory.java
@@ -21,14 +21,20 @@
 
 package org.perfmon4j.demo;
 
+import java.util.Properties;
+import java.util.Random;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.perfmon4j.PerfMon;
 import org.perfmon4j.PerfMonConfiguration;
-import org.perfmon4j.TextAppender;
+import org.perfmon4j.PerfMonTimer;
+import org.perfmon4j.influxdb.InfluxAppender;
 import org.perfmon4j.instrument.SnapShotGauge;
 import org.perfmon4j.instrument.SnapShotProvider;
+import org.perfmon4j.instrument.SnapShotString;
+import org.perfmon4j.java.management.JVMSnapShot;
 
 /**
  * This is an example of a very simple SnapShotMonitor...  
@@ -39,7 +45,13 @@ import org.perfmon4j.instrument.SnapShotProvider;
  */
 @SnapShotProvider(type = SnapShotProvider.Type.STATIC)
 public class JVMMemory {
-   
+
+	
+	@SnapShotString
+	public static String getUserName() {
+		return System.getProperty("user.name");
+	}
+	
 	@SnapShotGauge
 	public static long getFreeMemory() {
 		return Runtime.getRuntime().freeMemory();
@@ -49,23 +61,69 @@ public class JVMMemory {
 	public static long getTotalMemory() {
 		return Runtime.getRuntime().totalMemory();
 	}
+	
+	private static class Runner implements Runnable {
+		private final Random r = new Random();
+		public void run() {
+			while (true) {
+				PerfMonTimer timer = PerfMonTimer.start("RandomRunner");
+				try {
+					Thread.sleep(r.nextInt(10) * 50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+				} finally {
+					PerfMonTimer.stop(timer);
+				}
+				try {
+					Thread.sleep(r.nextInt(10) * 100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+				}
+			}
+		}
+		
+	}
+	
     
     public static void main(String args[]) throws Exception {
-    	System.setProperty("PERFMON_APPENDER_ASYNC_TIMER_MILLIS", "500");
+//    	System.setProperty("PERFMON_APPENDER_ASYNC_TIMER_MILLIS", "500");
     	
     	BasicConfigurator.configure();
         Logger.getRootLogger().setLevel(Level.INFO);
         Logger.getLogger("org.perfmon4j").setLevel(Level.DEBUG);
-   	
     	
         PerfMonConfiguration config = new PerfMonConfiguration();
-        config.defineAppender("SimpleAppender", TextAppender.class.getName(), "1 second");
-        config.defineSnapShotMonitor("JVM Memory", JVMMemory.class.getName());
-        config.attachAppenderToSnapShotMonitor("JVM Memory", "SimpleAppender");
+        Properties props = new Properties();
+        props.setProperty("baseURL", "http://192.168.56.1:9099");
+        props.setProperty("database", "perfmon4j");
+        props.setProperty("groups", "\"my group\" other\\group=test");
+//        props.setProperty("batchSeconds", "30");
+        
+        
+//        props.setProperty("maxMeasurementsPerBatch", "2");
+        
+        config.defineAppender("SimpleAppender", InfluxAppender.class.getName(), "1 second", props);
+        config.defineMonitor("RandomRunner");
+        
+        config.attachAppenderToMonitor("RandomRunner", "SimpleAppender");
+        
+        
+        config.defineSnapShotMonitor("Daves=Cool Test", JVMMemory.class.getName());
+        config.attachAppenderToSnapShotMonitor("Daves=Cool Test", "SimpleAppender");
+
+        config.defineSnapShotMonitor("NewJVM", JVMSnapShot.class.getName());
+        config.attachAppenderToSnapShotMonitor("NewJVM", "SimpleAppender");
         
         
         PerfMon.configure(config);
-        System.out.println("Sleeping for 5 seconds -- Will take a JVM SnapShot every 1 second");
-        Thread.sleep(5000);
+        System.out.println("Sleeping for 60 seconds -- Will take a JVM SnapShot every 1 second");
+        
+        for (int i = 0; i < 10; i++) {
+        	Thread t = new Thread(new Runner());
+        	t.setDaemon(true);
+        	t.start();	
+        }
+        
+        Thread.sleep(600000);
     }
 }
