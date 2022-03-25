c0d40a1879c263765a9bee4fa03430728fb4ab57
David Deuchert <david@deuchert.com>
2022-03-25 17:17:01 -0500

    New utility
    This utility will be used in the Influx appender to allow categories to be grouped together under the same name.  For reporting a subCategory tag will be added to the measurement in InfluxDb

---
+++ base/src/main/java/org/perfmon4j/util/SubCategorySplitter.java
@@ -0,0 +1,87 @@
+/*
+ *	Copyright 2022 Follett School Solutions, LLC 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ *  Follett School Solutions, LLC
+ *  1340 Ridgeview Drive
+ *  McHenry, IL 60050
+ * 
+*/
+
+package org.perfmon4j.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class SubCategorySplitter {
+	private static final Logger logger = LoggerFactory.initLogger(SubCategorySplitter.class);
+	
+	private static final Pattern leadingAndTrailingPeriods = Pattern.compile("^\\.*|\\.*$");
+	private static final Pattern multiplePeriods = Pattern.compile("\\.{2,}");
+	private final Pattern pattern;
+	
+	public SubCategorySplitter(String regEx) {
+		Pattern tmp = null;
+		try {
+			tmp = Pattern.compile(regEx);
+		} catch (PatternSyntaxException ex) {
+			logger.logWarn("Invalid Sub Category Splitter pattern found and will be ignored: " + regEx);
+		}
+		pattern = tmp;
+	}
+	
+	public Split split(String category) {
+		if (pattern != null) {
+			Matcher m = pattern.matcher(category);
+			
+			if (m.find()) {
+				String subCategory = m.group(1);
+				category = category.substring(0, m.start(1)) + "." + category.substring(m.end(1), category.length());
+				return new Split(fixupPeriods(category), fixupPeriods(subCategory));
+			} else {
+				return new Split(category, null);
+			}
+		} else {
+			return new Split(category, null);
+		}
+	}
+	
+	private String fixupPeriods(String category) {
+		category = leadingAndTrailingPeriods.matcher(category).replaceAll("");
+		return multiplePeriods.matcher(category).replaceAll(".");
+	}
+	
+	public class Split {
+		private final String category;
+		private final String subCategory;
+		
+		private Split(String category, String subCategory) {
+			this.category = category;
+			this.subCategory = subCategory;
+		}
+
+		public String getCategory() {
+			return category;
+		}
+
+		public String getSubCategory() {
+			return subCategory;
+		}
+		
+		public boolean hasSubCategory() {
+			return subCategory != null;
+		}
+	}
+}
---
+++ base/src/test/java/org/perfmon4j/util/SubCategorySplitterTest.java
@@ -0,0 +1,121 @@
+/*
+ *	Copyright 2022 Follett School Solutions, LLC 
+ *
+ *	This file is part of PerfMon4j(tm).
+ *
+ * 	Perfmon4j is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU Lesser General Public License, version 3,
+ * 	as published by the Free Software Foundation.  This program is distributed
+ * 	WITHOUT ANY WARRANTY OF ANY KIND, WITHOUT AN IMPLIED WARRANTY OF MERCHANTIBILITY,
+ * 	OR FITNESS FOR A PARTICULAR PURPOSE.  You should have received a copy of the GNU Lesser General Public 
+ * 	License, Version 3, along with this program.  If not, you can obtain the LGPL v.s at 
+ * 	http://www.gnu.org/licenses/
+ * 	
+ * 	perfmon4j@fsc.follett.com
+ * 	David Deuchert
+ *  Follett School Solutions, LLC
+ *  1340 Ridgeview Drive
+ *  McHenry, IL 60050
+ * 
+*/
+
+package org.perfmon4j.util;
+
+import org.perfmon4j.util.SubCategorySplitter.Split;
+
+import junit.framework.TestCase;
+
+public class SubCategorySplitterTest extends TestCase {
+
+	public void testCategoryMatch() throws Exception {
+    	String category = "DistrictRequest.blue";
+    	SubCategorySplitter splitter = new SubCategorySplitter("DistrictRequest\\.(.*)");
+
+    	Split split = splitter.split(category);
+    	
+    	assertNotNull("Should always get back a split object", split);
+    	
+    	assertTrue("Should have a subCategory", split.hasSubCategory());
+    	assertEquals("Expected subCategory after the split", "blue", split.getSubCategory());
+    	assertEquals("Expected category after the split", "DistrictRequest", split.getCategory());
+    }
+
+    public void testNoMatch() throws Exception {
+    	String category = "WebRequest.blue";
+    	SubCategorySplitter splitter = new SubCategorySplitter("DistrictRequest\\.(.*)");
+
+    	Split split = splitter.split(category);
+    	
+    	assertNotNull("Should always get back a split object", split);
+    	
+    	assertFalse("Should NOT have a subCategory", split.hasSubCategory());
+    	assertNull("subCategory should be null", split.getSubCategory());
+    	assertEquals("Expect category to be unchanged", category, split.getCategory());
+    }
+ 
+    public void testInnerMatch() throws Exception {
+    	String category = "DistrictRequest.dist_0001.circulation";
+    	SubCategorySplitter splitter = new SubCategorySplitter("DistrictRequest\\.([^\\.]*).*");
+
+    	Split split = splitter.split(category);
+    	
+    	assertNotNull("Should always get back a split object", split);
+    	
+    	assertTrue("Should have a subCategory", split.hasSubCategory());
+    	assertEquals("Expected subCategory after the split", "dist_0001", split.getSubCategory());
+    	assertEquals("Expected category after the split", "DistrictRequest.circulation", split.getCategory());
+    }
+    
+    public void testInvalidRegEx() throws Exception {
+    	// If you create a splitter with an invalid regex it should 
+    	// perform as a no-op function.  
+    	
+    	String category = "DistrictRequest.blue";
+    	String badRegEx = "[DistrictRequest\\.(.*)";
+    	SubCategorySplitter splitter = new SubCategorySplitter(badRegEx);
+
+    	Split split = splitter.split(category);
+    	
+    	assertNotNull("Should always get back a split object", split);
+    	
+    	assertFalse("Should NOT have a subCategory since splitter was invalid", split.hasSubCategory() || (split.getCategory() == null));
+    	assertEquals("Expected category since splitter was invalid", "DistrictRequest.blue", split.getCategory());
+    }
+    
+    public void fixupPeriodsOnSubCategory() throws Exception {
+    	String category = "DistrictRequest.dist_0001.circulation";
+    	
+    	// This is not a great pattern, because it will leave a subcategory that starts with a
+    	// period.
+    	String notAGreatPattern = "DistrictRequest(.*)";
+    	SubCategorySplitter splitter = new SubCategorySplitter(notAGreatPattern);
+
+    	Split split = splitter.split(category);
+    	
+    	assertEquals("Expected subCategory after the split", "dist_0001.circulation", split.getSubCategory());
+    	assertEquals("Expected category after the split", "DistrictRequest", split.getCategory());
+    }
+
+    public void fixupPeriodsOnConcatBeforeAfterSubCategory() throws Exception {
+    	String category = "DistrictRequest.dist_0001.circulation";
+    	
+    	// This is NOT a great pattern so we will help.
+    	// It will result in multiple periods when the two
+    	// sections before and after the match are concatenated.
+    	// What the user probably wants out of this is:
+    	// 		category = DistrictRequest.circulation
+    	// 		subCategory =  dist001
+    	// But what they would get, if we didn't fix up the result
+    	// after the regex was applied, would be:
+    	// 		category = DistrictRequest..circulation
+    	// 		subCategory =  dist001
+    	
+    	String notAGreatPattern = "DistrictRequest\\.(dist_\\d{4}).*";
+    	SubCategorySplitter splitter = new SubCategorySplitter(notAGreatPattern);
+
+    	Split split = splitter.split(category);
+    	
+    	assertEquals("Expected subCategory after the split", "dist_0001", split.getSubCategory());
+    	assertEquals("Expected category after the split", "DistrictRequest.circulation", split.getCategory());
+    }
+}
