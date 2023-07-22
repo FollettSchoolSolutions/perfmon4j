3a856b0e05aac2fdcd1174146f3b2cdd7a88fb3e
David Deuchert <david@deuchert.com>
2023-07-22 18:15:28 -0500

    Initial push

---
+++ base/src/main/java/org/perfmon4j/SnapShotPOJORegistry.java
@@ -0,0 +1,243 @@
+package org.perfmon4j;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class SnapShotPOJORegistry {
+	private static final SnapShotPOJORegistry singleton = new SnapShotPOJORegistry();
+	
+	private final Object entriesLockToken = new Object();
+	private final Map<String, RegistryEntry> entries = new HashMap<String, SnapShotPOJORegistry.RegistryEntry>(); 
+	private static final POJOInstance[] EMPTY_INSTANCES = new POJOInstance[] {};
+	
+	public static SnapShotPOJORegistry getSingleton() {
+		return singleton;
+	}
+
+	SnapShotPOJORegistry() {
+	}
+
+	public void register(Object snapShotPOJO) {
+		register(snapShotPOJO, true);
+	}
+
+	public void register(Object snapShotPOJO, boolean weakReference) {
+		synchronized(entriesLockToken) {
+			final String className = snapShotPOJO.getClass().getName();
+			
+			RegistryEntry entry = entries.get(className);
+			if (entry == null) {
+				entry = new RegistryEntry(className);
+				entries.put(className, entry);
+			}
+			entry.addOrUpdateInstance(snapShotPOJO, weakReference);
+		}
+	}
+	
+	public void deRegister(Object snapShotPOJO) {
+		synchronized(entriesLockToken) {
+			final String className = snapShotPOJO.getClass().getName();
+			
+			RegistryEntry entry = entries.get(className);
+			if (entry != null) {
+				if (entry.removeInstance(snapShotPOJO)) {
+					entries.remove(className);
+				}
+			}
+		}
+	}
+	
+	
+	public static class RegistryEntry {
+		private final String className;
+		private final Object instancesLockToken = new Object();
+		private final Map<String, Object> instances = new HashMap<String, Object>(); 
+		
+		RegistryEntry(String className) {
+			this.className = className;
+		}
+
+		public String getClassName() {
+			return className;
+		}
+		
+		void addOrUpdateInstance(Object pojo, boolean weakReference) {
+			final String instanceName = "bogus"; // = pojo.getInstanceName();
+			synchronized (instancesLockToken) {
+				instances.put(instanceName, weakReference ? new WeakPOJOInstance(instanceName, pojo) : new StrongPOJOInstance(instanceName, pojo));
+			}
+		}
+		
+		boolean removeInstance(Object pojo) {
+			String instanceName = "bogus";
+			synchronized (instancesLockToken) {
+				instances.remove(instanceName);
+				return instances.isEmpty();
+			}
+		}
+		
+		public POJOInstance[] getInstances() {
+			synchronized (instancesLockToken) {
+				return instances.values().toArray(EMPTY_INSTANCES);
+			}
+		}
+	}
+
+	public POJOInstance[] getInstances(String className) {
+		RegistryEntry entry = null;
+		
+		synchronized (entriesLockToken) {
+			entry = entries.get(className);
+		}
+		
+		return entry != null ? entry.getInstances() : EMPTY_INSTANCES;
+	}
+	
+	public abstract static class POJOInstance {
+		private static final AtomicLong nextPojoID = new AtomicLong(); 
+		
+		private final long pojoID = nextPojoID.incrementAndGet(); 
+		private final String instanceName;
+		
+		protected POJOInstance(String instanceName) {
+			this.instanceName = instanceName;
+		}
+		
+		public String getInstanceName() {
+			return instanceName;
+		}
+		
+		abstract Object getPOJO();
+		
+	    boolean isActive() {
+	    	return getPOJO() != null;
+	    }
+		
+		public long getPojoID() {
+			return pojoID;
+		}
+
+	    public SnapShotData initSnapShot(long currentTimeMillis) {
+	    	return null;
+	    }
+	    
+	    
+	/*----------------------------------------------------------------------------*/    
+	    /**
+	     * @param data - Returns SnapShotData returned by initSnapShot() or
+	     * null if initSnapShot() did not return a SnapShotData.
+	     * @param currentTimeMillis - Current System Time
+	     * @return - Return a SnapShotData instance that will be passed to the appender.
+	     */
+	    public SnapShotData takeSnapShot(SnapShotData data, long currentTimeMillis) {
+	    	return null;
+	    }
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(instanceName, Long.valueOf(pojoID));
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			POJOInstance other = (POJOInstance) obj;
+			return Objects.equals(instanceName, other.instanceName) && pojoID == other.pojoID;
+		}
+	}
+	
+	private static class StrongPOJOInstance extends POJOInstance {
+		private final Object pojo;
+		
+		StrongPOJOInstance(String instanceName, Object pojo) {
+			super(instanceName);
+			this.pojo = pojo;
+		}
+
+		public Object getPOJO() {
+			return pojo;
+		}
+	}
+	
+	private static class WeakPOJOInstance extends POJOInstance {
+		private final WeakReference<Object> pojoReference;
+		
+		WeakPOJOInstance(String instanceName, Object pojo) {
+			super(instanceName);
+			this.pojoReference = new WeakReference<Object>(pojo);
+		}
+
+		public Object getPOJO() {
+			return pojoReference.get();
+		}
+	}
+	
+	
+//    private static class MultiMonitorTimerTask extends FailSafeTimerTask {
+//    	private final Map<SnapShotManager, V>
+//    	private final WeakReference<RegistryEntry> entry;
+//        private final AppenderID appenderID;
+//        private final long intervalMillis;
+//        private final boolean usePriorityTimer;
+//        
+//        MultiMonitorTimerTask(RegistryEntry entry, AppenderID appenderID, boolean usePriorityTimer) throws InvalidConfigException {
+//            this.entry = new WeakReference(entry);
+//            this.appenderID = appenderID;
+//            this.usePriorityTimer = usePriorityTimer;
+//            intervalMillis = appenderID.getIntervalMillis();
+//            Timer timerToUse = usePriorityTimer ? PerfMon.getPriorityTimer() : PerfMon.getUtilityTimer();
+//            long now = MiscHelper.currentTimeWithMilliResolution();
+//            timerToUse.schedule(this, PerfMon.roundInterval(now, intervalMillis));
+//        }
+//        
+//        public void failSafeRun() {
+//            Appender appender = Appender.getAppender(appenderID);
+//            SnapShotMonitor monitor = (SnapShotMonitor)monitorReference.get();
+//
+//            boolean cancelTask = true;
+//            if (monitor != null && monitor.isActive() && PerfMon.isConfigured()) {
+//                try {
+//                    data = monitor.takeSnapShot(data, MiscHelper.currentTimeWithMilliResolution());
+//                } catch (Exception ex) {
+//                    data = null;
+//                    logger.logError("Error taking snap shot for monitor: " + monitor.getName(), ex);
+//                }
+//                if (appender != null) {
+//                    cancelTask = false;
+//                    if (data != null) {
+//                        data.setName(monitor.getName());
+//                        try {
+//                            appender.appendData(data);
+//                        } catch (Exception ex) {
+//                            logger.logError("Error appending snapshot data for monitor: " + monitor.getName(), ex);
+//                        }
+//                    }
+//                    try {
+//                    	// Reschedule task
+//                        new MonitorTimerTask(monitor, appenderID,  usePriorityTimer);
+//                    } catch (Exception ex) {
+//                        data = null;
+//                        logger.logError("Error in initSnapShot for monitor: " + monitor.getName(), ex);
+//                    }
+//                }
+//            } 
+//            
+//            if (cancelTask) {
+//                if (monitor != null) {
+//                    synchronized(monitor.lockToken) {
+//                        monitor.map.remove(appenderID);
+//                    }
+//                }
+//            }
+//        }
+//    }
+	
+}
---
+++ base/src/test/java/org/perfmon4j/SnapShotPOJORegistryTest.java
@@ -0,0 +1,98 @@
+package org.perfmon4j;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.perfmon4j.SnapShotPOJORegistry.POJOInstance;
+import org.perfmon4j.instrument.SnapShotPOJO;
+import org.perfmon4j.instrument.SnapShotCounter;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+public class SnapShotPOJORegistryTest extends TestCase {
+	
+	@SnapShotPOJO
+	public static class POJO {
+		@SnapShotCounter
+		public long getCounter() {
+			return 0;
+		}
+	}
+	
+	public void testWeakReferenceRegistration() throws Exception {
+		SnapShotPOJORegistry registry = new SnapShotPOJORegistry();
+		final String pojoClassName = POJO.class.getName(); 
+		
+		SnapShotPOJORegistry.POJOInstance[] pojoInstances = registry.getInstances(pojoClassName);
+		assertNotNull(pojoInstances);
+		assertEquals("pojoInstances.length", 0, pojoInstances.length);
+		
+		POJO pojo = new POJO();
+		registry.register(pojo, true);
+		
+		pojoInstances = registry.getInstances(pojoClassName);
+		assertEquals("pojoInstances.length", 1, pojoInstances.length);
+		
+		POJOInstance instance = pojoInstances[0];
+		assertTrue("Should be active as long as it is not dereferrenced", instance.isActive());
+		
+		pojo = null;
+		System.gc();
+		
+		assertFalse("Should no longer be active once it is dreferenced", instance.isActive());
+	}
+	
+	public void testStrongReferenceRegistration() throws Exception {
+		SnapShotPOJORegistry registry = new SnapShotPOJORegistry();
+		final String pojoClassName = POJO.class.getName(); 
+		
+		POJO pojo = new POJO();
+		registry.register(pojo, false);
+		
+		SnapShotPOJORegistry.POJOInstance[] pojoInstances = registry.getInstances(pojoClassName);
+		assertEquals("pojoInstances.length", 1, pojoInstances.length);
+		
+		POJOInstance instance = pojoInstances[0];
+		assertTrue("Should be active as long as it is not dereferrenced", instance.isActive());
+		
+		pojo = null;
+		System.gc();
+		
+		assertTrue("Since it was not registered as a weak reference, registry should maintain a strong reference", instance.isActive());
+	}
+	
+    
+/*----------------------------------------------------------------------------*/    
+    public static void main(String[] args) {
+        String[] testCaseName = {SnapShotManagerTest.class.getName()};
+        
+        BasicConfigurator.configure();
+        Logger.getLogger(SnapShotPOJORegistryTest.class.getPackage().getName()).setLevel(Level.DEBUG);
+        
+        
+        TestRunner.main(testCaseName);
+    }
+
+/*----------------------------------------------------------------------------*/
+    public static junit.framework.Test suite() {
+        String testType = System.getProperty("UNIT");
+        TestSuite newSuite = new TestSuite();
+
+        // Here is where you can specify a list of specific tests to run.
+        // If there are no tests specified, the entire suite will be set in the if
+        // statement below.
+//        newSuite.addTest(new SnapShotManagerTest("testSimpleGetMonitorKeyWithFieldsFromMonitorWithInstances"));
+//        newSuite.addTest(new SnapShotManagerTest("testSimpleGetMonitorKeyWithStringField"));
+        
+        // Here we test if we are running testunit or testacceptance (testType will
+        // be set) or if no test cases were added to the test suite above, then
+        // we run the full suite of tests.
+        if (testType != null || newSuite == null || (newSuite.countTestCases() < 1)) {
+            newSuite = new TestSuite(SnapShotPOJORegistryTest.class);
+        }
+
+        return(newSuite);
+    }
+}
